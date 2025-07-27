1d17fd6f00e39efa3c0362c9478d4dd2c1d2a5e2
sujon <jasujon570@gmail.com>
2025-07-27 20:27:18 +0600

    BMI Calculator using if else condition

--- BMI_Calculator.js
+++ BMI_Calculator.js
@@ -11,3 +11,20 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
     - Otherwise, you are obese.
 
 */
+
+let weight = 105;
+let height = 1.71;
+
+let BMI = (weight / (height*2));
+console.log(BMI.toFixed(2));
+
+
+if (BMI < 18.5) {
+    console.log("You are underweight");
+}else if (BMI >= 18.5 && BMI <= 24.9) {
+    console.log("You are normal");
+}else if (BMI  >= 25 && BMI <= 29.9) {
+    console.log("You are overweight");
+}else {
+    console.log("You are obese");
+}
