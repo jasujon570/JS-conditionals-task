9c14cd77d5d7925f5fda317395b8024f897d6e8e
sujon <jasujon570@gmail.com>
2025-07-27 21:19:45 +0600

    another ternary version used

--- BMI_Calculator.js
+++ BMI_Calculator.js
@@ -12,13 +12,13 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
 
 */
 
-let weight = 105;
+let weight = 85;
 let height = 1.71;
 
 let BMI = (weight / (height*2));
 console.log(BMI.toFixed(2));
 
-
+// if esle condition
 if (BMI < 18.5) {
     console.log("You are underweight");
 }else if (BMI >= 18.5 && BMI <= 24.9) {
@@ -28,3 +28,10 @@ if (BMI < 18.5) {
 }else {
     console.log("You are obese");
 }
+
+// ternary operator
+
+BMI < 18.5 ? console.log("You are underweight") : (BMI >= 18.5 && BMI <= 24.9) ? console.log("You are normal") : (BMI  >= 25 && BMI <= 29.9) ? console.log("You are overweight") : console.log("You are obese");
+
+let resutl = BMI < 18.5 ? "You are underweight" : (BMI <= 24.9 ? "You are normal" : BMI <= 29.9 ? "You are overweight" : "You are obese")
+console.log(resutl);
