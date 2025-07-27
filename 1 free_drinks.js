216a7a1c5ebce6fbd354f5669a9a95f27f82fec9
sujon <jasujon570@gmail.com>
2025-07-27 19:53:35 +0600

    conditions check

---
+++ 1 free_drinks.js
@@ -0,0 +1,14 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burger = 500;
+
+if (burger > 500) {
+    log.console(Free Coke);
+} else {
+    log.console(Coke: 30tk);
+}
