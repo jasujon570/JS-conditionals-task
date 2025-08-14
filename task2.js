6a3b108c708a05208b55ff08f0220340dd9be058
sujon <jasujon570@gmail.com>
2025-08-14 12:57:12 +0600

    practice about && operator

---
+++ task2.js
@@ -0,0 +1,10 @@
+// JavaScript multiple condition practice using && ||
+
+const age = 19;
+const height = 61;
+
+if(age > 18 && height > 60){
+    console.log('He will push the car')
+} else {
+    console.log('He will seating on the car')
+}
