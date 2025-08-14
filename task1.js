b22e1b602e6d5bfd16534575b633c03474ebaa04
sujon <jasujon570@gmail.com>
2025-08-14 12:53:36 +0600

    practice about if-else conditions

---
+++ task1.js
@@ -0,0 +1,47 @@
+const backHome = 7;
+if(backHome < 6 ){
+    console.log('Mother will give me breakfast')
+}else {
+    console.log('Mother will beat me with jharo!')
+}
+
+const logedIn = false;
+
+if(logedIn == true){
+    console.log('Welcome!')
+} else {
+    console.log('Get Lost!')
+}
+
+const run = 4;
+
+if(run === 5) {
+    console.log('I will give you chocolate for eat')
+} else {
+    console.log('I will obese')
+}
+
+const mark = 85;
+
+if(mark > 80){
+    console.log('Father will buy a bike for me')
+} else {
+    console.log('Father will not give me a bike')
+}
+
+
+const showTime = 10;
+
+if(showTime < 9) {
+    console.log('I will watch the movie')
+} else {
+    console.log('I will go back to home and sleep')
+}
+
+const temparature = 20;
+
+if(temparature >= 30) {
+    console.log('I will turn on air conditioner')
+} else {
+    console.log('I will sleep with blanket')
+}
